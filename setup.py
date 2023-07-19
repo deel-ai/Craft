ecd058ebae255fe7b3eea597ef883919db333b78
Thomas FEL <thomas_fel@brown.edu>
2023-07-19 17:37:09 -0400

    Bump version: 0.0.1 → 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", encoding="utf-8") as fh:
 
 setup(
     name="Craft-xai",
-    version="0.0.1",
+    version="0.0.2",
     description="Automatic Concept Extraction with CRAFT",
     long_description=README,
     long_description_content_type="text/markdown",
